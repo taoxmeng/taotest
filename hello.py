18b6181bffecbc3275ae28dd5804c245c27af1a4
taoxmeng <taoxmeng@gmail.com>
2023-11-27 09:25:36 -0500

    test

--- hello.py
+++ hello.py
@@ -1,1 +1,22 @@
-print ("hello")
+nodes = {'A': ['C', 'E', 'B', 'X'],
+         'B': ['C', 'D'],
+         'C': ['D','T','R'],
+         'D': ['F'],
+         'F': ['G','H']
+         }
+
+def find_dependencies(target, dependencies):
+        dependencies = dependencies + [target]
+        if target not in nodes.keys():
+            return dependencies
+        for node in nodes[target]:
+            if node not in dependencies:
+                newdependencies = find_dependencies(node, dependencies)
+                for node in newdependencies:
+                    if node not in dependencies: 
+                        dependencies = dependencies + [node]
+        return dependencies
+    
+
+
+print (find_dependencies('A',[]))
